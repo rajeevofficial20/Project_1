fb6e25ef92c6eaf07d490f44b64562addbd2c449
Rajeev kumar <rajeevkumar20497@gmail.com>
2026-03-20 14:06:13 +0530

    Add files via upload

---
+++ Project_1.py
@@ -0,0 +1,17 @@
+a = float(input("enter  first number"))
+b = float(input("enter second number"))
+sum= a+b
+multiply= a*b
+divide= (a/b)
+power_value=a**b
+substract=a-b
+area_of_rectangle=2*(a+b)
+print(sum)
+print(multiply)
+print(divide)
+print(power_value)
+print(substract)
+print(area_of_rectangle)
+
+
+
